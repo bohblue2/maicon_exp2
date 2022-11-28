fdbfe5103e6de64080ac48448360e19210a46325
-edup <fuck>
2022-11-29 02:23:10 +0900

    Remove Inverting

--- yolov5-pip/yolov5/utils/augmentations.py
+++ yolov5-pip/yolov5/utils/augmentations.py
@@ -37,7 +37,6 @@ class Albumentations:
                 A.RandomBrightnessContrast(p=0.0),
                 A.RandomGamma(p=0.0),
                 A.ImageCompression(quality_lower=75, p=0.0),
-                A.InvertImg(p=1)
             ]  # transforms
             self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
 
