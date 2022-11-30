36b35c18463932ea0e9c038e3e0c46eb2f19e120
bohblue23 <bohblue23@gmail.com>
2022-12-01 00:22:19 +0900

    Change root dir of model artifacts

--- w_download_model.py
+++ w_download_model.py
@@ -2,7 +2,7 @@ import wandb
 project = 'maicon_all'
 entity = 'ryanbae'
 run = wandb.init(project=project)
-model = run.use_artifact(f'{entity}/{project}/run_ir:latest', type='model')
-directory = model.download()
-model = run.use_artifact(f'{entity}/{project}/run_thermal:latest', type='model')
-directory = model.download()
+model = run.use_artifact(f'{entity}/{project}/run_ir:v0', type='model')
+directory = model.download(root='yolov5-pip/weights/ir')
+model = run.use_artifact(f'{entity}/{project}/run_thermal:v0', type='model')
+directory = model.download(root='yolov5-pip/weights/thermal')
