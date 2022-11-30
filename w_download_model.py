e0d3f0a05c6fb68a26e268349f04bdd5ee0f7550
root <root@MAICON-P017.localdomain>
2022-11-30 23:10:49 +0900

    Refactor

--- w_download_model.py
+++ w_download_model.py
@@ -1,6 +1,8 @@
 import wandb
-run = wandb.init(project='maicon_all', entity='ryanbae')
-model = run.use_artifact('run_ir:latest')
+project = 'maicon_all'
+entity = 'ryanbae'
+run = wandb.init(project=project)
+model = run.use_artifact(f'{entity}/{project}/run_ir:latest', type='model')
 directory = model.download()
-model = run.use_artifact('run_thermal:latest')
+model = run.use_artifact(f'{entity}/{project}/run_thermal:latest', type='model')
 directory = model.download()
